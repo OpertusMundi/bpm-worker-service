7a04c7e2f8afccbc360362a2dfbee04382c2aff8
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-05-27 13:57:32 +0300

    Add external task for updating purchased assets

---
+++ bpm-worker/src/main/java/eu/opertusmundi/bpm/worker/subscriptions/payin/UpdateConsumerAssetsTaskService.java
@@ -0,0 +1,76 @@
+package eu.opertusmundi.bpm.worker.subscriptions.payin;
+
+import java.util.UUID;
+
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import eu.opertusmundi.bpm.worker.model.BpmnWorkerException;
+import eu.opertusmundi.bpm.worker.subscriptions.AbstractTaskService;
+import eu.opertusmundi.common.model.ServiceException;
+import eu.opertusmundi.common.service.OrderFulfillmentService;
+
+@Service
+public class UpdateConsumerAssetsTaskService extends AbstractTaskService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UpdateConsumerAssetsTaskService.class);
+
+    @Value("${opertusmundi.bpm.worker.tasks.update-consumer-assets.lock-duration:10000}")
+    private Long lockDurationMillis;
+
+    @Autowired
+    private OrderFulfillmentService orderFulfillmentService;
+    
+    @Override
+    public String getTopicName() {
+        return "updateConsumerAssets";
+    }
+
+    @Override
+    protected long getLockDuration() {
+        return this.lockDurationMillis;
+    }
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        try {
+            final String taskId = externalTask.getId();
+
+            logger.info("Received task. [taskId={}]", taskId);
+
+            // Get parameters
+            final UUID payInKey = UUID.fromString(externalTask.getBusinessKey());
+
+            logger.debug("Processing task. [taskId={}, externalTask={}]", taskId, externalTask);
+
+            this.orderFulfillmentService.updateConsumer(payInKey);
+
+            // Complete task
+            externalTaskService.complete(externalTask);
+
+            logger.info("Completed task. [taskId={}]", taskId);
+        } catch (final BpmnWorkerException ex) {
+            logger.error(String.format("Operation has failed. [details=%s]", ex.getErrorDetails()), ex);
+            
+            externalTaskService.handleFailure(
+                externalTask, ex.getMessage(), ex.getErrorDetails(), ex.getRetries(), ex.getRetryTimeout()
+            );
+        } catch (final ServiceException ex) {
+            logger.error(String.format("Operation has failed. [details=%s]", ex.getMessage()), ex);
+            
+            externalTaskService.handleFailure(
+                externalTask, ex.getMessage(), ex.getMessage(), DEFAULT_RETRY_COUNT, DEFAULT_RETRY_TIMEOUT
+            );
+        } catch (final Exception ex) {
+            logger.error(DEFAULT_ERROR_MESSAGE, ex);
+
+            this.handleError(externalTaskService, externalTask, ex);
+        }
+    }
+    
+}
