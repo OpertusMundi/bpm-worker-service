1a0349bfd5e959009b4954c47a6ec9a559ef38d5
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-07-18 16:15:07 +0300

    Update system maintenance task services
    Delete asset statistics data, when deleting orphan catalogue items.

--- bpm-worker/src/main/java/eu/opertusmundi/bpm/worker/subscriptions/support/RemoveOrphanCatalogueItemsTaskService.java
+++ bpm-worker/src/main/java/eu/opertusmundi/bpm/worker/subscriptions/support/RemoveOrphanCatalogueItemsTaskService.java
@@ -8,18 +8,25 @@ import java.util.UUID;
 import org.apache.commons.io.FileUtils;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 import eu.opertusmundi.bpm.worker.subscriptions.AbstractTaskService;
 import eu.opertusmundi.common.domain.AccountEntity;
+import eu.opertusmundi.common.model.BasicMessageCode;
+import eu.opertusmundi.common.model.ServiceException;
 import eu.opertusmundi.common.model.catalogue.client.CatalogueAssetQuery;
 import eu.opertusmundi.common.model.catalogue.server.CatalogueFeature;
 import eu.opertusmundi.common.repository.AccountRepository;
 import eu.opertusmundi.common.service.CatalogueService;
+import eu.opertusmundi.common.service.ElasticSearchService;
 import eu.opertusmundi.common.service.IngestService;
 import eu.opertusmundi.common.util.StreamUtils;
 
@@ -43,6 +50,9 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
     @Autowired
     private IngestService ingestService;
 
+    @Autowired
+    private ElasticSearchService elasticSearchService;
+    
     @Override
     protected long getLockDuration() {
         return this.lockDurationMillis;
@@ -71,6 +81,8 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
                     if (publisher == null) {
                         logger.info("Removing asset due to missing publisher [pid={}, publisher={}]", pid, publisherKey);
 
+                        this.deleteAssetStatistics(externalTask, externalTaskService, pid);
+                        
                         StreamUtils.from(f.getProperties().getIngestionInfo()).forEach(d -> {
                             this.ingestService.removeLayerAndData(d.getTableName(), null, null);
                         });
@@ -103,5 +115,25 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
             this.handleFailure(externalTaskService, externalTask, ex);
         }
     }
+    
+    private void deleteAssetStatistics(ExternalTask externalTask, ExternalTaskService externalTaskService, String pid) {
+        try {
+            final SearchSourceBuilder searchBuilder = new SearchSourceBuilder();
+            final BoolQueryBuilder    query         = QueryBuilders.boolQuery();
+            query.should(QueryBuilders.matchQuery("id.keyword", pid));
+            query.minimumShouldMatch(1);
+            final String entity = searchBuilder.query(query).toString();
+
+            this.elasticSearchService.performRequest(HttpMethod.POST, "/assets_view/_delete_by_query", entity);
+            this.elasticSearchService.performRequest(HttpMethod.POST, "/assets_view_aggregate/_delete_by_query", entity);
+        } catch (Exception ex) {
+            final String message = String.format("Failed to delete asset statistics [pid=%s]", pid);
+            throw new ServiceException(BasicMessageCode.InternalServerError, message, ex);
+        } finally {
+            // Extend lock duration
+            externalTaskService.extendLock(externalTask, this.getLockDuration());
+        }
+    }
+    
 }
  
