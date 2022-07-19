92ac33e2ce5497a8906f830685f0f8447f56f79c
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-07-19 08:58:54 +0300

    Update system maintenance task services
    Delete assets after scanning the catalogue to avoid missing
    records due to pagination over deleted entries.

--- bpm-worker/src/main/java/eu/opertusmundi/bpm/worker/subscriptions/support/RemoveOrphanCatalogueItemsTaskService.java
+++ bpm-worker/src/main/java/eu/opertusmundi/bpm/worker/subscriptions/support/RemoveOrphanCatalogueItemsTaskService.java
@@ -1,7 +1,9 @@
 package eu.opertusmundi.bpm.worker.subscriptions.support;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,6 +31,8 @@ import eu.opertusmundi.common.service.CatalogueService;
 import eu.opertusmundi.common.service.ElasticSearchService;
 import eu.opertusmundi.common.service.IngestService;
 import eu.opertusmundi.common.util.StreamUtils;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Service
 public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
@@ -38,21 +42,30 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
     @Value("${opertusmundi.bpm.worker.tasks.remove-orphan-catalogue-items.lock-duration:120000}")
     private Long lockDurationMillis;
 
-    @Autowired
-    private Path assetDirectory;
+    @Value("${opertusmundi.bpm.worker.tasks.remove-orphan-catalogue-items.batch-size:20}")
+    private int batchSize;
+
+    private final AccountRepository    accountRepository;
+    private final CatalogueService     catalogueService;
+    private final ElasticSearchService elasticSearchService;
+    private final IngestService        ingestService;
+    private final Path                 assetDirectory;
 
     @Autowired
-    private AccountRepository accountRepository;
+    public RemoveOrphanCatalogueItemsTaskService(
+        AccountRepository accountRepository, 
+        CatalogueService catalogueService,
+        ElasticSearchService elasticSearchService, 
+        IngestService ingestService, 
+        Path assetDirectory
+    ) {
+        this.accountRepository    = accountRepository;
+        this.catalogueService     = catalogueService;
+        this.elasticSearchService = elasticSearchService;
+        this.ingestService        = ingestService;
+        this.assetDirectory       = assetDirectory;
+    }
 
-    @Autowired
-    private CatalogueService catalogueService;
-
-    @Autowired
-    private IngestService ingestService;
-
-    @Autowired
-    private ElasticSearchService elasticSearchService;
-    
     @Override
     protected long getLockDuration() {
         return this.lockDurationMillis;
@@ -66,37 +79,23 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
     @Override
     public final void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
         try {
-            final String  taskId            = externalTask.getId();
-
+            final String taskId = externalTask.getId();
             logger.info("Received task. [taskId={}]", taskId);
 
-            CatalogueAssetQuery    query    = CatalogueAssetQuery.builder().page(0).size(20).build();
-            List<CatalogueFeature> features = this.catalogueService.findAllFeatures(query);
+            // Collect assets to delete
+            CatalogueAssetQuery     query    = CatalogueAssetQuery.builder().page(0).size(batchSize).build();
+            List<CatalogueFeature>  features = this.catalogueService.findAllFeatures(query);
+            List<FeatureDeleteTask> tasks    = new ArrayList<>();
 
             while (!features.isEmpty()) {
-                for (final CatalogueFeature f : features) {
-                    final String        pid          = f.getId();
-                    final UUID          publisherKey = f.getProperties().getPublisherId();
+                for (final CatalogueFeature feature : features) {
+                    final String        pid          = feature.getId();
+                    final UUID          publisherKey = feature.getProperties().getPublisherId();
                     final AccountEntity publisher    = this.accountRepository.findOneByKey(publisherKey).orElse(null);
                     if (publisher == null) {
                         logger.info("Removing asset due to missing publisher [pid={}, publisher={}]", pid, publisherKey);
-
-                        this.deleteAssetStatistics(externalTask, externalTaskService, pid);
                         
-                        StreamUtils.from(f.getProperties().getIngestionInfo()).forEach(d -> {
-                            this.ingestService.removeLayerAndData(d.getTableName(), null, null);
-                        });
-
-                        final Path assetPath = Paths.get(this.assetDirectory.toString(), f.getId());
-                        final Path draftPath = assetPath.toFile().exists() ? assetPath.toRealPath() : null;
-                        if (assetPath.toFile().exists()) {
-                            FileUtils.deleteQuietly(assetPath.toFile());
-                        }
-                        if (draftPath != null && draftPath.toFile().exists()) {
-                            FileUtils.deleteQuietly(draftPath.toFile());
-                        }
-
-                        this.catalogueService.unpublish(publisherKey, pid);
+                        tasks.add(FeatureDeleteTask.of(feature, pid, publisherKey));
                     }
                 }
                 // Fetch next batch
@@ -104,10 +103,18 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
                 features = this.catalogueService.findAllFeatures(query);
                 // Extend lock duration
                 externalTaskService.extendLock(externalTask, this.getLockDuration());
-            }      
+            }
             
-            externalTaskService.complete(externalTask);
+            // Delete assets
+            for (final FeatureDeleteTask task : tasks) {
+                this.deleteAssetStatistics(externalTask, externalTaskService, task);
+                this.deleteAsset(externalTask, externalTaskService, task);
+                
+                // Extend lock duration
+                externalTaskService.extendLock(externalTask, this.getLockDuration());
+            }
 
+            externalTaskService.complete(externalTask);
             logger.info("Completed task. [taskId={}]", taskId);
         } catch (final Exception ex) {
             logger.error(DEFAULT_ERROR_MESSAGE, ex);
@@ -116,23 +123,51 @@ public class RemoveOrphanCatalogueItemsTaskService extends AbstractTaskService {
         }
     }
     
-    private void deleteAssetStatistics(ExternalTask externalTask, ExternalTaskService externalTaskService, String pid) {
+    private void deleteAsset(ExternalTask externalTask, ExternalTaskService externalTaskService, FeatureDeleteTask task) {
+        StreamUtils.from(task.feature.getProperties().getIngestionInfo()).forEach(d -> {
+            this.ingestService.removeLayerAndData(d.getTableName(), null, null);
+        });
+
+        try {
+            final Path assetPath = Paths.get(this.assetDirectory.toString(), task.feature.getId());
+            final Path draftPath = assetPath.toFile().exists() ? assetPath.toRealPath() : null;
+            if (assetPath.toFile().exists()) {
+                FileUtils.deleteQuietly(assetPath.toFile());
+            }
+            if (draftPath != null && draftPath.toFile().exists()) {
+                FileUtils.deleteQuietly(draftPath.toFile());
+            }
+        } catch (IOException ex) {
+            logger.info(String.format("Failed to delete asset resources [pid=%s]", task.pid), ex);
+        }
+        
+        this.catalogueService.unpublish(task.publisherKey, task.pid);
+    }
+    
+    private void deleteAssetStatistics(ExternalTask externalTask, ExternalTaskService externalTaskService, FeatureDeleteTask task) {
         try {
             final SearchSourceBuilder searchBuilder = new SearchSourceBuilder();
             final BoolQueryBuilder    query         = QueryBuilders.boolQuery();
-            query.should(QueryBuilders.matchQuery("id.keyword", pid));
+            query.should(QueryBuilders.matchQuery("id.keyword", task.pid));
             query.minimumShouldMatch(1);
             final String entity = searchBuilder.query(query).toString();
 
             this.elasticSearchService.performRequest(HttpMethod.POST, "/assets_view/_delete_by_query", entity);
             this.elasticSearchService.performRequest(HttpMethod.POST, "/assets_view_aggregate/_delete_by_query", entity);
         } catch (Exception ex) {
-            final String message = String.format("Failed to delete asset statistics [pid=%s]", pid);
+            final String message = String.format("Failed to delete asset statistics [pid=%s]", task.pid);
             throw new ServiceException(BasicMessageCode.InternalServerError, message, ex);
-        } finally {
-            // Extend lock duration
-            externalTaskService.extendLock(externalTask, this.getLockDuration());
         }
+    }
+
+    @AllArgsConstructor(staticName = "of")
+    @Builder
+    private static class FeatureDeleteTask {
+
+        private CatalogueFeature feature;
+        private String           pid;
+        private UUID             publisherKey;
+
     }
     
 }
